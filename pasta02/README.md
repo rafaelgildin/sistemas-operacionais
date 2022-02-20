45c342d439d9ccde047c47a95a12c8b122989f36
rafaelgildin <rafagildin@gmail.com>
2022-02-20 20:00:27 -0300

    tarefa 2 so

--- pasta02/README.md
+++ pasta02/README.md
@@ -1,10 +1,24 @@
 * Alunos: 
-* Rafael Avraham Gildin Acherboim - 31940242
-<!-- * Pedro Carvalho - 41910974 -->
+    * Rafael Avraham Gildin Acherboim - 31940242
+    * Pedro Carvalho - 41910974
 
-* Usamos a IDE DEV-C++ : https://sourceforge.net/projects/orwelldevcpp/
-* Como compilar o programa na plataforma disponibilizada: Pressionar a tecla F9
-* Como executar o programa: Pressionar a tecla F10
-* Como comprovar que os resultados propostos foram alcançados: Disponibilizamos a imagem da compilação e execução.
+* Usamos a Visual Studio Code : https://code.visualstudio.com/
+* Como compilar e executar o programa na plataforma disponibilizada: Pressionar o botão triangular "run code", no canto superior direito da IDE.
+* Como comprovar que os resultados propostos foram alcançados: Disponibilizamos as imagens de compilação e execução.
 
-<!-- ![Alt text](ex.png?raw=true "Tarefa") -->
+* Comando cat:
+    * Comando cat executado com sucesso:
+    ![Alt text](imagens/cat_ok.png?raw=true "Comando cat executado com sucesso:")
+    * Comando cat executado com falha:
+    ![Alt text](imagens/cat_ok.png?raw=true "Comando cat executado com falha:")
+
+* Comando cp:
+    * Comando cp executado com sucesso:
+    ![Alt text](imagens/cp_ok.png?raw=true "Comando cp executado com sucesso:")
+    * Comando cp executado com falha, devido ao nome do arquivo de entrada:
+    ![Alt text](imagens/cp_entrada_nok.png?raw=true "Comando cp executado com falha, devido ao nome do arquivo de entrada:")
+    * Comando cp executado com falha, devido ao nome do arquivo de saída:
+    ![Alt text](imagens/cp_saida_nok.png?raw=true "Comando cp executado com falha, devido ao nome do arquivo de saída:")
+
+* Comando exit:
+    ![Alt text](imagens/exit.png?raw=true "Comando exit:")
--- pasta02/main.c
+++ pasta02/main.c
@@ -1,65 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <stdbool.h>
 
-int principal(void){
-    // pegar o nome do arquivo
-    char nome_arquivo[250];
-    printf("Digite o nome do arquivo: ");
-    scanf("%s", &nome_arquivo);
-    printf("Nome do arquivo = %s\n", nome_arquivo);
+int cp(void){
+    FILE *arquivo1, *arquivo2_leitura, *arquivo2_escrita; // aponta para os arquivos
+    char nome_arquivo_1[50], nome_arquivo_2[50];
 
-    FILE *entrada;
-    entrada = fopen (nome_arquivo, "r");//abre o arquivo
-    if (entrada == NULL) {
-        printf ("\nNao encontrei o arquivo!\n");
-        exit (EXIT_FAILURE);
-    }   
-    double soma = 0.0;
-    int n = 0;
-    while (true) {
-        int x; 
-        int k = fscanf (entrada, "%d", &x);
-        if (k != 1) break;
-        soma += x;
-        n += 1;
-    }
-    fclose (entrada);//fecha o arquivo
-    printf ("A media dos numeros e %f\n", soma/n);
-    return EXIT_SUCCESS;
-}
+    printf("Arquivo de entrada: ");
+    scanf("%s", nome_arquivo_1);
+    printf("Arquivo de saida: ");
+    scanf("%s", nome_arquivo_2);
+    
+	//le os dois arquivos
+	arquivo1 = fopen(nome_arquivo_1, "r");
+    arquivo2_leitura = fopen(nome_arquivo_2, "r");
 
-int leitura_teclado (void) {
-   char linha[100];
-   int n = 0;
-   while (true) {
-      linha[n] = getchar ();
-      if (linha[n] == '\n') break;
-      n = n + 1;
-   }
-//   for (int i = 0; i <= n; i += 1)
-//    putchar (linha[i]);
-   return EXIT_SUCCESS;
+    //faz a checagem para achar os arquivos
+	if (arquivo1 == NULL){
+		printf("O arquivo de entrada nao foi encontrado.\n");
+        return 0;
+	}
+    if (arquivo2_leitura != NULL){
+		printf("Ja existe um arquivo com o nome do arquivo de saida.\n");
+        return 0;
+	}
+    arquivo2_escrita = fopen(nome_arquivo_2, "w");
+
+	//itera por um arquivo e vai escrevendo no outro ate chegar no fim do arquivo(EOF)
+    char caractere;
+	while((caractere=fgetc(arquivo1)) != EOF){
+		fputc(caractere,arquivo2_escrita);
+	};
+	//fecha os dois arquivos
+    fclose(arquivo1);
+	fclose(arquivo2_escrita);
+    fclose(arquivo2_leitura);
+    printf("Arquivo copiado com sucesso!\n");
+    return 0;
 }
 
 
-int leitura_arquivo (void) {
+int cat (void) {
     FILE* ptr;
-    char ch;
- 
+    char ch, nome_arquivo[50];
+
+    printf("nome do arquivo: ");
+    scanf("%s", nome_arquivo);
+
     // Abrindo o arquivo no modelo de leitura
-    ptr = fopen("dados.txt", "r");
+    ptr = fopen(nome_arquivo, "r");
  
     if (NULL == ptr) {
         printf("O arquivo nao pode ser aberto\n");
+        return 0;
     }
- 
-    printf("O conteudo do arquivo eh: \n");
  
     // printar o que esta escrito no arquivo
     // caracter por caracter usando o loop
+    printf("O conteudo do arquivo eh: \n");
     do {
         ch = fgetc(ptr);
         printf("%c", ch);
@@ -73,31 +72,10 @@ int leitura_arquivo (void) {
     return 0;
 }
 
-
-void cp(char* arquivos[]){
-	// aponta para os arquivos
-	FILE *arquivo1, *arquivo2;
-	//le os dois arquivos
-	arquivo1 = fopen(arquivos[1], "r");
-	arquivo2 = fopen(arquivos[2], "w");
-	//faz a checagem para achar os arquivos
-	if (arquivo1 == NULL || arquivo2 == NULL){
-		printf("Um dos arquivos, ou ambos, infelizmente não pode ser aberto =c ");
-	}
-	//itera por um arquivo e vai escrevendo no outro até chegar no fim do arquivo(EOF)
-	char caractere;
-	while((caractere=fgetc(arquivo1)) != EOF){
-		fputc(caractere,arquivo2);
-	};
-	//fecha os dois arquivos
-	fclose(arquivo1);
-	fclose(arquivo2);
-}
-
 void entrada() {
     while(true){
         
-        printf("####################################################################\n");
+        printf("\n\n\n####################################################################\n");
         printf("#################  COMPILADOR DE SISTEMAS OPERACIONAIS  ############\n");
         printf("####################################################################\n\n");
         
@@ -109,40 +87,28 @@ void entrada() {
         printf("*******************************************************************\n\n");
 
         char comando[50];
-        // printf("$");
-        // scanf("%s", comando);
+        printf("$ ");
+        scanf("%s", comando);
         // printf("Comando = %s\n", comando); //digitar o comando
 
-        // if(comando == "cat"){
-        //     leitura_arquivo();
-        //     break;
-        // }
-        if(comando == "cp"){
-        	char nome_arquivo_1[250];
-        	char nome_arquivo_2[250];
-        	printf("Digite o nome do arquivo que você quer copiar: \n");
-        	scanf("%s", &nome_arquivo_1);
-        	printf("Digite o nome do arquivo para onde você quer copiar: \n");
-        	scanf("%s", &nome_arquivo_2);
-        	cp([nome_arquivo_1, nome_arquivo_2])
-		}
-        // else{
-        //     printf("comando invalido!! \n\n\n\n\n\n\n\n");
-        // }
-        break;
+        if(!strcmp(comando,"exit")){
+            printf("Saindo do compilador :(\nVolte sempre :)\n\n");
+            break;
+        }
+        else if(!strcmp(comando,"cat")){
+            cat();
+        }
+        else if(!strcmp(comando,"cp")){
+            cp();
+        }
+        else{
+            printf("comando invalido!! \n\n\n\n");
+        }
     }
 }
 
 
 int main () {
-    /*
-    exercicio: file:///C:/Users/rafaa/git/sistemas-operacionais/pasta02/aula02_lab.pdf
-    ler 
-    https://cboard.cprogramming.com/c-programming/48313-if-statements-using-strings-condition.html
-    geeksforgeeks.org/c-program-to-read-contents-of-whole-file/
-    https://www.geeksforgeeks.org/basic-input-and-output-in-c/
-    https://flaviocopes.com/c-null/
-    */
     entrada();
     return 0;
 }
